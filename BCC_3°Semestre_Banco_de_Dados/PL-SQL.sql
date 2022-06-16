8590467c24858b585e80ae681425ce43a10e4ce0
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-06-15 23:20:22 -0300

    Program Language SQL

--- BCC_3°Semestre_Banco_de_Dados/PL-SQL.sql
+++ BCC_3°Semestre_Banco_de_Dados/PL-SQL.sql
@@ -129,15 +129,115 @@ DECLARE /*Declarações (opcional)*/
     v_empno employees.employee_id%TYPE;
     v_lane employees.Last_name%type;
 BEGIN /*Operações SQL e PL/SQL (obrigatório)*/
-    OPEN emp_cursor; /*Abre o cursor IS/AS, ou seja, busca as linhas retornadas pelas consultas*/
+    OPEN emp_cursor; /*Abre o cursor IS que foi declarado, ou seja, busca as linhas retornadas pelas consultas*/
         LOOP /*Inicia um loop*/
             FETCH emp_cursor INTO v_empno, v_lane; /*Pesquisar as linhas de cada coluna*/
             EXIT WHEN emp_cursor%NOTFOUND; /*para a pesquisa quando nada mais for encontrado nas colunas*/
             DBMS_OUTPUT.PUT_LINE(v_empno || ' ' || v_lane) /*escreve as linhas de cada coluna no buffer*/
         END LOOP; /*finaliza o loop*/
-    CLOSE emp_cursor; /*Fecha cursor IS/AS*/
+    CLOSE emp_cursor; /*Fecha o cursor IS*/
 END; /*(obrigatório)*/
 
 
-/* (STORAGE PROCEDURE)________________________________________________________________________________________________________________________________________
+/* (STORED PROCEDURE)________________________________________________________________________________________________________________________________________
+    Procedimento armazenado ou Stored Procedure é uma Sub-Rotina do tipo procedimento que contém uma coleção de comandos em SQL, que podem 
+    ser executadas em um Banco de dados de uma só vez. Os Storeds Procedures encapsulam tarefas repetitivas, aceitam parâmetros de entrada, 
+    são capazes de utilizar os comandos como IF e ELSE, WHILE, LOOP, REPEAT e CASE, além de poderem chamar outros Storeds Procedures e podem 
+    imprimir um valor de status.
+    
+    Os parâmetros passados para a Sub-Rotina-procedimento (Stored Procedure) podem ser de 3 tipos:
+        - IN:       (padrão) passa um valor do escopo principal (fora da Sub-Rotina) para a Sub-Rotina e 
+                    esse valor não pode ser alterado dentro da Sub-Rotina.
+
+        - OUT:      passa um valor da Sub-Rotina para o escopo principal (fora da Sub-Rotina).
+
+        - IN OUT:   passa um valor do escopo principal(fora da Sub-Rotina) para a Sub-Rotina. Esse valor 
+                    pode ser alterado dentro da Sub-Rotina e imprimido com o valor atualizado para o escopo 
+                    principal (fora da Sub-Rotina).
+
+    Sintaxe:
+        CREATE OR REPLACE PROCEDURE 
+            Nome_Proc (Argumento1 modo Tipo_de_Dados, Argumento2 modo Tipo_de_Dados, Argumento3 modo Tipo_de_Dados)
+                IS ou AS
+                Variáveis locais, constantes,...
+                BEGIN
+                    Bloco Pl/SQL
+                    Exception
+            End nome_da_procedure;*/
+
+
+/*Ex: Sub-Rotina-Procedimento (Stored Procedure) — não há retorno —:*/
+SET SERVEROUTPUT ON 
+CREATE OR REPLACE PROCEDURE /*A Cláusula REPLACE faz com que uma função seja recriada se ela já existir.*/
+    Nr_Fone (V_fone in varchar2)
+        IS
+        NV_Fone varchar2(10)
+        BEGIN
+            NV_Fone:= Substr(V_fone,1,4)||'-'||Substr(V_fone,5,4);
+            Dbms_Output.Put_Line (NV_Fone);
+    END Nr_Fone;
+/*Executando a Stored Procedure*/
+exec nr_fone(12345678);/*output: 1234-5678*/
+
+
+/*Ex: Sub-Rotina-Procedimento (Stored Procedure) — não há retorno —:*/
+CREATE OR REPLACE PROCEDURE /*A Cláusula REPLACE faz com que uma função seja recriada se ela já existir.*/
+    Testa_Parametros (X IN number, Y OUT number, Z IN OUT number)
+        IS
+        BEGIN
+            Y:=X*2;
+            Y:= Y+Z;
+            Z:=Y+X+Z;
+    END Testa_Parametros;
+/*Executando a Stored Procedure*/
+Set serveroutput on 
+DECLARE
+    Var1 number;
+    Var2 number;
+    Var3 number;
+BEGIN
+    Var1:=30;
+    Var2:=40;
+    Var3:=50;
+    Dbms_Output.Put_Line(Var1||'-'||Var2||'-'||Var3);/*(X=30, Y=40, Z=50)*/
+    Testa_Parametros(Var1,Var2,Var3);/*Stored Procedure: entra(X=30, Y=40, Z=50)→ sai(X=30, Y=110, Z=190)*/
+    Dbms_Outpu.Put_Line(Var1||'-'||Var2||'-'||Var3);/*(X=30, Y=110, Z=190)*/
+END;
+
+
+/* (STORED FUNCTION)___________________________________________________________________________________________
+    Função armazenada(também chamada de função do usuário ou função definida pelo usuário) ou Stored Function é 
+    uma Sub-Rotina do tipo função, o seja, tem por objetivo retornar algum resultado ou retornar algum valor. 
+
+    Sintaxe (parecida com a da Stored Procedure, com exceção do RETURNO que apenas as functions possuem):
+    CREATE OR REPLACE FUNCTION 
+        Nome_Funçao (Argumento1 modo Tipo_de_Dados, Argumento2 modo Tipo_de_Dados, Argumento3 modo Tipo_de_Dados)
+            RETURN Tipo_de_Dado
+            IS ou AS
+            Variáveis locais, constantes,...
+            BEGIN
+                Bloco Pl/SQL
+                Exception
+        END nome_da_função
+
+    obs: Não se usa Stored Function nos seguintes casos:
+    • Em uma cláusula de restrição CHECK de um CREATE TABLE ou ALTER TABLE; 
+    • Em uma cláusula DEFAULT de uma instrução CREATE TABLE ou ALTER TABLE.
+    • Em uma consulta-DQL ou instrução-DML, a função não pode ter OUT ou IN OUT como parâmetros
 */
+
+/*Ex: Sub-Rotina-Função (Stored Function) — há retorno —:*/
+CREATE OR REPLACE FUNCTION /*A Cláusula REPLACE faz com que uma função seja recriada se ela já existir.*/
+    balanco (numcont IN NUMBER) 
+        RETURN NUMBER /*declara o tipo do retorno*/
+        IS 
+        contabalanco NUMBER(11,2);
+        BEGIN 
+            SELECT saldo_conta 
+            INTO contabalanco 
+            FROM pedidos 
+            WHERE pedido_conta = numcont; 
+            RETURN(contabalanco); /*retorno da função(retorna o saldo da conta especificada)*/
+    END balanco;
+/*Executando a Stored Function*/
+SELECT balanco(12345) from dual /*12345 é o número da conta escolhida para a busca de seu saldo.*/
