64b0cce8ea9610390190683baa4df5153c656667
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-01-09 20:39:40 -0300

    Vetor/Array

--- 3° Semestre/Um Guia Didático - Estruturas de dados Estáticas/VetorArray.java
+++ 3° Semestre/Um Guia Didático - Estruturas de dados Estáticas/VetorArray.java
@@ -1,205 +1,258 @@
-import java.sql.Array;
 import java.util.Scanner; // importação da classe Scanner do pacote java.util
 
-//Declaração, alocação e operações — de forma estática na memória — de Vetor (ou Array) de estrutura de dados primitivos, do mesmo tipo (estrutura homogênea), ou não primitivos:
+//Declaração, alocação e operações — de forma estática na memória — de Vetor/Array de estrutura de dados primitivos (do mesmo tipo → estrutura homogênea), ou não primitivos:
 public class VetorArray {
+	public Scanner sc = new Scanner(System.in);
 
-	public VetorArray () {
-		Scanner sc = new Scanner(System.in);
-        
+	public VetorArray() {
 		System.out.print("Tamanho: ");
 		int tamanho = sc.nextInt();
-		assert(tamanho>0): "O tamanho do vetor precisa ser maior que zero";
+		assert (tamanho > 0) : "O tamanho do vetor precisa ser maior que zero";
 
 		System.out.print("Tipo de dados: digite 'p' (para primitivos → [byte, short, int, long, float, double, char, boolean] ou 'np' (para não primitivos → [string] ...");
 		String tipo_de_dado = sc.nextLine();
-		assert(tipo_de_dado == "p" || tipo_de_dado == "np"): "Aceita-se 'p' ou 'np' como parâmetro";
-		
+		assert (tipo_de_dado == "p" || tipo_de_dado == "np") : "Aceita-se 'p' ou 'np' como parâmetro";
+
 		if (tipo_de_dado == "p") {
 			System.out.print("Defina o tipo primitivo (byte, short, int, long, float, double, char, boolean): ");
-        	String tipo = sc.nextLine();
+			String tipo = sc.nextLine();
 
 			if (tipo == "byte") {
-				//Declaração e Alocação estaticamente do vetor/array arranjo na memória:
-				byte array[] = new byte[tamanho];
-				System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-				
-				for (int i = 0; i < array.length; i++) {
-					System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
-					array[i] = sc.nextByte();
-					if (i == array.length - 1) {
-						System.out.print("Arranjo definido, todas as células foram preenchidas.");
-					}
-				}
+				getByteArray(tamanho, tipo, sc);
 			} else if (tipo == "short") {
-				//Declaração e Alocação estaticamente do vetor/array arranjo na memória:
-				short array[] = new short[tamanho];
-				System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-
-				for (int i = 0; i < array.length; i++) {
-					System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
-					array[i] = sc.nextShort();
-					if (i == array.length - 1) {
-						System.out.print("Arranjo definido, todas as células foram preenchidas.");
-					}
-				}
+				getShortArray(tamanho, tipo, sc);
 			} else if (tipo == "int") {
-				//Declaração e Alocação estaticamente do vetor/array arranjo na memória:
-				int array[] = new int[tamanho];
-				System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-
-				for (int i = 0; i < array.length; i++) {
-					System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
-					array[i] = sc.nextInt();
-					if (i == array.length - 1) {
-						System.out.print("Arranjo definido, todas as células foram preenchidas.");
-					}
-				}
+				getIntArray(tamanho, tipo, sc);
 			} else if (tipo == "long") {
-				//Declaração e Alocação estaticamente do vetor/array arranjo na memória:
-				long array[] = new long[tamanho];
-				System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-
-				for (int i = 0; i < array.length; i++) {
-					System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
-					array[i] = sc.nextLong();
-					if (i == array.length - 1) {
-						System.out.print("Arranjo definido, todas as células foram preenchidas.");
-					}
-				}
+				getLongArray(tamanho, tipo, sc);
 			} else if (tipo == "float") {
-				//Declaração e Alocação estaticamente do vetor/array arranjo na memória:
-				float array[] = new float[tamanho];
-				System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-
-				for (int i = 0; i < array.length; i++) {
-					System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
-					array[i] = sc.nextFloat();
-					if (i == array.length - 1) {
-						System.out.print("Arranjo definido, todas as células foram preenchidas.");
-					}
-				}
+				getFloatArray(tamanho, tipo, sc);
 			} else if (tipo == "double") {
-				//Declaração e Alocação estaticamente do vetor/array arranjo na memória:
-				double array[] = new double[tamanho];
-				System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-
-				for (int i = 0; i < array.length; i++) {
-					System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
-					array[i] = sc.nextDouble();
-					if (i == array.length - 1) {
-						System.out.print("Arranjo definido, todas as células foram preenchidas.");
-					}
-				}
+				getDoubleArray(tamanho, tipo, sc);
 			} else if (tipo == "char") {
-				//Declaração e Alocação estaticamente do vetor/array arranjo na memória:
-				char array[] = new char[tamanho];
-				System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-
-				for (int i = 0; i < array.length; i++) {
-					System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
-					array[i] = sc.next().charAt(0);
-					if (i == array.length - 1) {
-						System.out.print("Arranjo definido, todas as células foram preenchidas.");
-					}
-				}
+				getCharArray(tamanho, tipo, sc);
 			} else if (tipo == "boolean") {
-				//Declaração e Alocação estaticamente do vetor/array arranjo na memória:
-				boolean array[] = new boolean[tamanho];
-				System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-
-				for (int i = 0; i < array.length; i++) {
-					System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
-					array[i] = sc.nextBoolean();
-					if (i == array.length - 1) {
-						System.out.print("Arranjo definido, todas as células foram preenchidas.");
-					}
-				}
+				getBooleanArray(tamanho, tipo, sc);
 			}
 		} else if (tipo_de_dado == "np") {
-			//Declaração e Alocação estaticamente do vetor/array arranjo na memória:
-			String array[] = new String[tamanho];
-			System.out.printf("Vetor do tipo de dados string de ", tamanho, " células alocado.");
+				getStringArray(tamanho, sc);
+		}
+	}
 
-			for (int i = 0; i < array.length; i++) {
-				System.out.printf("Defina o elemento da célula de índice ", i, "de tipo string:");
-				array[i] = sc.nextLine();
-				if (i == array.length - 1) {
-					System.out.print("Arranjo definido, todas as células foram preenchidas.");
-				}
+	public static byte[] getByteArray(int tamanho, String tipo, Scanner sc) {
+		// Declaração e Alocação estaticamente do vetor/array arranjo na memória:
+		byte array[] = new byte[tamanho];
+		System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
+			array[i] = sc.nextByte();
+			if (i == array.length - 1) {
+				System.out.print("Arranjo definido, todas as células foram preenchidas.");
 			}
 		}
+
+		//operações (→ exemplo em String, linha 201)
+		sc.close();
+		return array;
+	}
+
+	public static short[] getShortArray(int tamanho, String tipo, Scanner sc) {
+		// Declaração e Alocação estaticamente do vetor/array arranjo na memória:
+		short array[] = new short[tamanho];
+		System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
+			array[i] = sc.nextShort();
+			if (i == array.length - 1) {
+				System.out.print("Arranjo definido, todas as células foram preenchidas.");
+			}
+		}
+
+		//operações (→ exemplo em String, linha 201)
+		sc.close();
+		return array;
+	}
+
+	public static int[] getIntArray(int tamanho, String tipo, Scanner sc) {
+		// Declaração e Alocação estaticamente do vetor/array arranjo na memória:
+		int array[] = new int[tamanho];
+		System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
+			array[i] = sc.nextInt();
+			if (i == array.length - 1) {
+				System.out.print("Arranjo definido, todas as células foram preenchidas.");
+			}
+		}
+
+		//operações (→ exemplo em String, linha 201)
+		sc.close();
+		return array;
+	}
+
+	public static long[] getLongArray(int tamanho, String tipo, Scanner sc) {
+		// Declaração e Alocação estaticamente do vetor/array arranjo na memória:
+		long array[] = new long[tamanho];
+		System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
+			array[i] = sc.nextLong();
+			if (i == array.length - 1) {
+				System.out.print("Arranjo definido, todas as células foram preenchidas.");
+			}
+		}
+
+		//operações (→ exemplo em String, linha 201)
+		sc.close();
+		return array;
+	}
+
+	public static float[] getFloatArray(int tamanho, String tipo, Scanner sc) {
+		// Declaração e Alocação estaticamente do vetor/array arranjo na memória:
+		float array[] = new float[tamanho];
+		System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
+			array[i] = sc.nextFloat();
+			if (i == array.length - 1) {
+				System.out.print("Arranjo definido, todas as células foram preenchidas.");
+			}
+		}
+
+		//operações (→ exemplo em String, linha 201)
+		sc.close();
+		return array;
+	}
+
+	public static double[] getDoubleArray(int tamanho, String tipo, Scanner sc) {
+		// Declaração e Alocação estaticamente do vetor/array arranjo na memória:
+		double array[] = new double[tamanho];
+		System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
+			array[i] = sc.nextDouble();
+			if (i == array.length - 1) {
+				System.out.print("Arranjo definido, todas as células foram preenchidas.");
+			}
+		}
+
+		//operações (→ exemplo em String, linha 201)
+		sc.close();
+		return array;
+	}
+
+	public static char[] getCharArray(int tamanho, String tipo, Scanner sc) {
+		// Declaração e Alocação estaticamente do vetor/array arranjo na memória:
+		char array[] = new char[tamanho];
+		System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
+			array[i] = sc.next().charAt(0);
+			if (i == array.length - 1) {
+				System.out.print("Arranjo definido, todas as células foram preenchidas.");
+			}
+		}
+
+		//operações (→ exemplo em String, linha 201)
+		sc.close();
+		return array;
+	}
+
+	public static boolean[] getBooleanArray(int tamanho, String tipo, Scanner sc) {
+		// Declaração e Alocação estaticamente do vetor/array arranjo na memória:
+		boolean array[] = new boolean[tamanho];
+		System.out.printf("Vetor do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("Defina o elemento da célula de índice ", i, "de tipo", tipo, ":");
+			array[i] = sc.nextBoolean();
+			if (i == array.length - 1) {
+				System.out.print("Arranjo definido, todas as células foram preenchidas.");
+			}
+		}
+
+		//operações (→ exemplo em String, linha 201)
+		sc.close();
+		return array;
+	}
+
+	public static String[] getStringArray(int tamanho, Scanner sc) {
+		// Declaração e Alocação estaticamente do vetor/array arranjo na memória:
+		String array[] = new String[tamanho];
+		System.out.printf("Vetor do tipo de dados string de ", tamanho, " células alocado.");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("Defina o elemento da célula de índice ", i, "de tipo string:");
+			array[i] = sc.nextLine();
+			if (i == array.length - 1) {
+				System.out.print("Arranjo definido, todas as células foram preenchidas.");
+			}
+		}
+
+		//___↓ operações ↓_________
+		System.out.println("Digite uma operação ou 'q' para finalizar: \n 's' → para soma de células \n 'b' → para buscar o índice de um valor \n 'ri' → para remover um índice \n 'rv' → para remover um valor \n  'i' → para inserir um valor em um índice" );
+		String operacao = sc.nextLine();
+		while (operacao != "q") { 
+			if (operacao == "s") {
+				System.out.println("Vetor/Array de tipo de dados inválido para esta operação.");
+			} else if (operacao == "b") {
+				System.out.print("Digite o valor a ser buscado o índice:");
+				String x = sc.nextLine();
+				for (int i = 0; i < array.length; i++) {
+					if (array[i] == x) {
+						System.out.println("O índice procurado do valor " + x + " é " + i);
+					} else {
+						System.out.println("Não existe índice correspondente, pois não há nenhuma célula com o valor" + x);
+					}
+				}	
+			} else if (operacao == "ri") {
+				System.out.print("Digite o índice a ser removido:");
+				int indice = sc.nextInt();
+				String valor_indice = array[indice];
+				for (int j = indice + 1; j < array.length; j++) {
+					array[j - 1] = array[j];
+				}
+				System.out.println("Valor " + valor_indice + "da célula de índice" + indice + "removido.");
+			} else if (operacao == "rv") {
+				System.out.print("Digite o valor a ser removido:");
+				String x = sc.nextLine();
+				for (int i = 0; i < array.length; i++) {
+					if (array[i] == x) {
+						String valor_indice = array[i];
+						for (int j = i + 1; j < array.length; j++) {
+							array[j - 1] = array[j];
+						}
+						System.out.println("Valor " + valor_indice + "da célula de índice" + i + "removido.");
+					} else {
+						System.out.println("Não há índice a ser removido, pois não há nenhuma célula com o valor" + x);
+					}
+				}
+			} else if (operacao == "i") {
+				System.out.print("Digite o valor a ser inserido:");
+				String valor = sc.nextLine();
+				System.out.print("Digite o índice no qual será inserido o valor:");
+				int indice = sc.nextInt();
+				for (int j = array.length - 1; j > indice; j--) {
+					array[j] = array[j - 1];
+				}
+				array[indice] = valor;
+				System.out.println("Valor " + valor + "inserido na célula de índice" + indice);
+			}
+
+			System.out.println("Digite uma operação ou 'q' para finalizar: \n 's' → para soma de células \n 'b' → para buscar o índice de um valor \n 'ri' → para remover um índice \n 'rv' → para remover um valor \n  'i' → para inserir um valor em um índice" );
+			String novaoperacao = sc.nextLine();
+			operacao = novaoperacao;
+		}
+		//___↑ operações ↑_________
 
 		sc.close();
-	}
-
-	public static void main(String[] args) {
-		//Exemplos de uso dos métodos
-		somaCelulas (array);
-		
-		float valor_para_busca = 1.618;
-		indice_retornado = buscaIndice (valor_para_busca, array); //buscou o índice do valor 1,618
-		indice_removido = removeIndice (indice_retornado, array); //removeu esse índice, ou seja, removeu o próprio valor 1,618
-		
-		valor_para_remocao = valor_para_busca;
-		valor_removido = removeValor (valor_para_remocao, array); //removeu o valr 1,618 sem precisar buscar o índice (o preocedimendo fez isso)
-		arraynsereValor (valor_removido, indice_retornado, array); //insere novamente o valor 1,618 que foi removido
-	}
-	
-	// Subrotina procedimento
-	// Soma os valores das células de um vetor
-	public static void somaCelulas(int array[]) {
-		int soma = 0;
-		try {
-			for (int i = 0; i < array.length; i++) {
-				System.out.println(array + "[" + i + "] = " + array[i]);
-				soma += array[i];
-			}
-			System.out.println("Soma: " + soma);
-		} catch (Exception e) {
-			System.out.println("Vetor/Array de tipo de dados inválido para esta operação.");
-		}
-	}
-
-	// Subrotina função
-	// Busca o índice i de um valor x em um vetor
-	public static int buscaIndice(int x, tipo array[]) {
-		for (int i = 0; i < nome.length; i++) {
-			if (array[i] == x) {
-				System.out.println("O índice procurado do valor " + x + " é " + i);
-				return i;
-			} else {
-				System.out.println("Não existe índice correspondente, pois não há nenhuma célula com o valor" + x);
-			}
-		}
-	}
-
-	// Subrotina função
-	// Remove o índice i de um vetor
-	public static tipo removeIndice(int i, tipo array[]) {
-		int valor_indice = array[i];
-		for (int j = i + 1; j < array.length; j++) {
-			array[j - 1] = array[j];
-		}
-		System.out.println("Valor " + valor_indice + "da célula de índice" + i + "removido.");
-		return valor_indice;
-	}
-
-	// Subrotina procedimento (chama subrotinas funções)
-	// Remove o valor x em qualquer indice i que for encontrado em um vetor
-	public static void removeValor(int x, tipo array[]) {
-		i = buscaIndice(x, array);
-		x = removeIndice(i, array);
-		System.out.println("Valor " + x + "da célula de índice" + i + "removido.");
-	}
-
-	// Subrotina procedimento
-	// Insere o valor x no índice i de um vetor
-	public static void insereValor(int x, int i, tipo array[]) {
-		for (int j = array.length - 1; j > i; j--) {
-			array[j] = array[j - 1];
-		}
-		array[i] = x;
-		System.out.println("Valor " + x + "inserido na célula de índice" + i);
+		return array;
 	}
 }
