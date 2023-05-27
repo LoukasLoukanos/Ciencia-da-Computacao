3a5e3fe5b7c24c232c3e9553947202c55e452e47
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-05-27 20:50:42 -0300

    Diretórios, Organização

--- Complementar - Estruturas de dados (Didático)/Lista.java
+++ Complementar - Estruturas de dados (Didático)/Lista.java
@@ -1,90 +1,60 @@
-import java.util.Scanner; // importação da classe Scanner do pacote java.util
+import java.util.Scanner;
 
 public class Lista<T> {
-    public Scanner sc = new Scanner(System.in);
-	private int tamanho;
-	private String tipo_de_dado;
-	private String tipo;
-	private T[] elementos;
+    private int tamanho;
+    private T[] elementos;
 
-	public static void main () {
-		
-	}
+    public static void main(String[] args) {
+        // Código adicional, se necessário
+    }
 
     public Lista(T[] elementos) {
         this.elementos = elementos;
     }
-    
-    public T get(int indice) {
-        return elementos[indice];
-    }
-    
+
     public void set(int indice, T elemento) {
         elementos[indice] = elemento;
     }
-	
-	/*
-	public T[] tipo_alocado (T tamanho) {
-		T elementos[] = new T[tamanho];
-	    Lista<T> lista = new Lista<T>(elementos);
-		System.out.printf("Lista do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-	}
-	*/
-	public void Aloca() {
-		System.out.print("Tamanho: ");
-		this.tamanho = sc.nextInt();
-		assert (tamanho > 0) : "O tamanho da lista precisa ser maior que zero!";
 
-		System.out.print("Tipo de dados: digite 'p' para primitivos → [byte, short, int, long, float, double, char, boolean] ou 'np' para não primitivos → [string] ...");
-		sc.nextLine();
-		this.tipo_de_dado = sc.nextLine();
-		assert (tipo_de_dado == "p" || tipo_de_dado == "np") : "Aceita-se 'p' ou 'np' como parâmetro!";
+    public T get(int indice) {
+        return elementos[indice];
+    }
 
-		if (tipo_de_dado == "p") {
-			System.out.print("Defina o tipo primitivo (byte, short, int, long, float, double, char, boolean): ");
-			sc.nextLine();
-			this.tipo = sc.nextLine();
-			
-			if (tipo == "byte") {
-				Byte elementos[] = new Byte[tamanho];
-	    		Lista<Byte> lista = new Lista<Byte>(elementos);
-				System.out.printf("Lista do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-			} else if (tipo == "short") {
-				Short elementos[] = new Short[tamanho];
-	    		Lista<Short> lista = new Lista<Short>(elementos);
-				System.out.printf("Lista do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-			} else if (tipo == "int") {
-				Integer elementos[] = new Integer[tamanho];
-	    		Lista<Integer> lista = new Lista<Integer>(elementos);
-				System.out.printf("Lista do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-			} else if (tipo == "long") {
-				Long elementos[] = new Long[tamanho];
-	    		Lista<Long> lista = new Lista<Long>(elementos);
-				System.out.printf("Lista do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-			} else if (tipo == "float") {
-				Float elementos[] = new Float[tamanho];
-	    		Lista<Float> lista = new Lista<Float>(elementos);
-				System.out.printf("Lista do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-			} else if (tipo == "double") {
-				Double elementos[] = new Double[tamanho];
-	    		Lista<Double> lista = new Lista<Double>(elementos);
-				System.out.printf("Lista do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-			} else if (tipo == "char") {
-				Character elementos[] = new Character[tamanho];
-	    		Lista<Character> lista = new Lista<Character>(elementos);
-				System.out.printf("Lista do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-			} else if (tipo == "boolean") {
-				Boolean elementos[] = new Boolean[tamanho];
-	    		Lista<Boolean> lista = new Lista<Boolean>(elementos);
-				System.out.printf("Lista do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-			}
-		} else if (tipo_de_dado == "np") {
-			String elementos[] = new String[tamanho];
-	    	Lista<String> lista = new Lista<String>(elementos);
-			System.out.printf("Lista do tipo de dados ", tipo, " de ", tamanho, " células alocado.");
-		}
-	}
-	public void Define(int indice) {
-        // ___↓ operações ↓_________
+    public void Aloca() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Tamanho: ");
+        this.tamanho = sc.nextInt();
+        assert (tamanho > 0) : "O tamanho da lista precisa ser maior que zero!";
+
+        System.out.print("Tipo de dados: digite 'p' para primitivos → [byte, short, int, long, float, double, char, boolean] ou 'np' para não primitivos → [string] ...");
+        sc.nextLine();
+        String tipo_de_dado = sc.nextLine();
+        assert (tipo_de_dado.equals("p") || tipo_de_dado.equals("np")) : "Aceita-se 'p' ou 'np' como parâmetro!";
+
+        if (tipo_de_dado.equals("p")) {
+            System.out.print("Defina o tipo primitivo (byte, short, int, long, float, double, char, boolean): ");
+            String tipo = sc.nextLine();
+
+            if (tipo.equals("byte") || tipo.equals("short") || tipo.equals("int") || tipo.equals("long") ||
+                    tipo.equals("float") || tipo.equals("double") || tipo.equals("char") || tipo.equals("boolean")) {
+                try {
+                    Class<?> clazz = Class.forName("java.lang." + tipo.substring(0, 1).toUpperCase() + tipo.substring(1));
+                    elementos = (T[]) Array.newInstance(clazz, tamanho);
+                    Lista<T> lista = new Lista<T>(elementos);
+                    System.out.printf("Lista do tipo de dados %s de %d células alocadas.\n", tipo, tamanho);
+                } catch (ClassNotFoundException e) {
+                    System.out.println("Tipo de dado inválido!");
+                }
+            } else {
+                System.out.println("Tipo de dado inválido!");
+            }
+        } else if (tipo_de_dado.equals("np")) {
+            elementos = (T[]) new String[tamanho];
+            Lista<T> lista = new Lista<T>(elementos);
+            System.out.printf("Lista do tipo de dados String de %d células alocadas.\n", tamanho);
+        } else {
+            System.out.println("Tipo de dado inválido!");
+        }
     }
 }
