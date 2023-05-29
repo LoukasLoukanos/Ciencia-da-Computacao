2cd976ec3975124fe5e0662f9a52f4972003500c
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-05-28 21:14:15 -0300

    Diretórios, Organização

--- Complementar - Estruturas de dados (Didático)/Lista.java
+++ Complementar - Estruturas de dados (Didático)/Lista.java
@@ -57,4 +57,84 @@ public class Lista<T> {
             System.out.println("Tipo de dado inválido!");
         }
     }
+
+    // ___↓ operações ↓_________
+	System.out.println(
+            "Digite uma operação ou 'q' para finalizar: \n 's' → para soma dos valores de todas as células \n 'b' → para buscar os índices de um valor \n 'ri' → para remover o valor da célula informando o índice \n 'rv' → para remover valor de célula(s) informando o valor\n  'i' → para inserir um valor em uma célula informando o índice");
+    sc.nextLine();
+    String operacao = sc.nextLine();
+    while (operacao != "q") {
+        if (operacao == "s") {
+            try {
+                int valor = array[0];
+                for (int i = 1; i < array.length; i++) {
+                    System.out.println(array + "[" + i + "] = " + array[i]);
+                    valor = valor + array[i];
+                }
+                System.out.println("Soma: " + valor);
+            } catch (Exception e) {
+                System.out.println("Vetor/Array de tipo de dados inválido para esta operação.");
+            }
+        } else if (operacao == "b") {
+            System.out.print("Digite o valor a ser buscado o índice: ");
+            sc.nextLine();
+            int x = sc.nextInt();
+            int count = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == x) {
+                    System.out.println("O índice" + i + " contém o valor " + x);
+                    count++;
+                }
+            }
+            if (count == 0) {
+                System.out.println("Não existe índice correspondente, pois não há nenhuma célula com o valor " + x);
+            }
+        } else if (operacao == "ri") {
+            System.out.print("Digite o índice da célula a ser removida: ");
+            sc.nextLine();
+            int indice = sc.nextInt();
+            int valor_indice = array[indice];
+            for (int j = indice + 1; j < array.length; j++) {
+                array[j - 1] = array[j];
+            }
+            System.out.println("Célula de índice " + indice + " de valor " + valor_indice + " removida.");
+        } else if (operacao == "rv") {
+            System.out.print("Digite o valor contido na(s) célula(s) a ser removida(s): ");
+            sc.nextLine();
+            int x = sc.nextInt();
+            int count = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == x) {
+                    int valor_indice = array[i];
+                    for (int j = i + 1; j < array.length; j++) {
+                        array[j - 1] = array[j];
+                    }
+                    System.out.println("Valor " + valor_indice + "da célula de índice" + i + "removido.");
+                    count++;
+                }
+            }
+            if (count == 0) {
+                System.out.println("Não há índice a ser removido, pois não há nenhuma célula com o valor" + x);
+            }
+        } else if (operacao == "i") {
+            System.out.print("Digite o valor a ser inserido:");
+            sc.nextLine();
+            int valor = sc.nextInt();
+            System.out.print("Digite o índice no qual será inserido o valor:");
+            sc.nextLine();
+            int indice = sc.nextInt();
+            for (int j = array.length - 1; j > indice; j--) {
+                array[j] = array[j - 1];
+            }
+            valor = array[indice];
+            System.out.println("Valor " + valor + "inserido na célula de índice" + indice);
+        }
+
+        System.out.println(
+                "Digite uma operação ou 'q' para finalizar: \n 's' → para soma dos valores de todas as células \n 'b' → para buscar os índices de um valor \n 'ri' → para remover o valor da célula informando o índice \n 'rv' → para remover valor de célula(s) informando o valor\n  'i' → para inserir um valor em uma célula informando o índice");
+        sc.nextLine();
+        String novaoperacao = sc.nextLine();
+        operacao = novaoperacao;
+    }
+    // ___↑ operações ↑_________
 }
