c4300081308aad6f530fc539badb2ab85cf6c439
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-06-27 19:56:39 -0300

    Algoritmos 08

---
+++ Complementar - Estruturas de dados (Didático)/Algoritmos08/Alg08.cpp
@@ -0,0 +1,77 @@
+/* Algoritmo da Estrutura de dados linear de Classificação/Ordenação estática (não dinâmica) de divisão e 
+   conquista MergeSort, também conhecida como Ordenação pelo Método (Recursivo) de Mistura/Mesclagem */
+
+#include <iostream>
+using namespace std;
+
+void merge(int array[], int metadeEsquerda[], int tamanhoEsquerda, int metadeDireita[], int tamanhoDireita) {
+    int i = 0; // Índice para a metade esquerda
+    int j = 0; // Índice para a metade direita
+    int k = 0; // Índice para o array final
+    
+    while (i < tamanhoEsquerda && j < tamanhoDireita) {
+        if (metadeEsquerda[i] <= metadeDireita[j]) {
+            array[k] = metadeEsquerda[i];
+            i++;
+        } else {
+            array[k] = metadeDireita[j];
+            j++;
+        }
+        k++;
+    }
+    
+    // Copiar os elementos restantes da metade esquerda
+    while (i < tamanhoEsquerda) {
+        array[k] = metadeEsquerda[i];
+        i++;
+        k++;
+    }
+    
+    // Copiar os elementos restantes da metade direita
+    while (j < tamanhoDireita) {
+        array[k] = metadeDireita[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int array[], int tamanho) {
+    if (tamanho <= 1) {
+        return;
+    }
+    
+    int meio = tamanho / 2;
+    int tamanhoEsquerda = meio;
+    int tamanhoDireita = tamanho - meio;
+    int metadeEsquerda[tamanhoEsquerda];
+    int metadeDireita[tamanhoDireita];
+    
+    // Preencher as duas metades
+    for (int i = 0; i < tamanhoEsquerda; i++) {
+        metadeEsquerda[i] = array[i];
+    }
+    for (int i = meio; i < tamanho; i++) {
+        metadeDireita[i - meio] = array[i];
+    }
+    
+    // Recursivamente ordenar as duas metades
+    mergeSort(metadeEsquerda, tamanhoEsquerda);
+    mergeSort(metadeDireita, tamanhoDireita);
+    
+    // Mesclar as metades ordenadas
+    merge(array, metadeEsquerda, tamanhoEsquerda, metadeDireita, tamanhoDireita);
+}
+
+int main() {
+    int array[] = {64, 34, 25, 12, 22, 11, 90};
+    int tamanho = sizeof(array) / sizeof(array[0]);
+    
+    mergeSort(array, tamanho);
+    
+    cout << "Array ordenado:" << endl;
+    for (int i = 0; i < tamanho; i++) {
+        cout << array[i] << " ";
+    }
+    
+    return 0;
+}
---
+++ Complementar - Estruturas de dados (Didático)/Algoritmos08/Alg08.py
@@ -0,0 +1,46 @@
+'''
+ Algoritmo da Estrutura de dados linear de Classificação/Ordenação estática (não dinâmica) de divisão e 
+ conquista MergeSort, também conhecida como Ordenação pelo Método (Recursivo) de Mistura/Mesclagem
+'''
+
+def mergeSort(array):
+    if len(array) <= 1:
+        return array
+    
+    meio = len(array) // 2
+    metadeEsquerda = array[:meio]
+    metadeDireita = array[meio:]
+    
+    metadeEsquerda = mergeSort(metadeEsquerda)
+    metadeDireita = mergeSort(metadeDireita)
+    
+    return merge(metadeEsquerda, metadeDireita)
+
+def merge(metadeEsquerda, metadeDireita):
+    array = []
+    i = 0  # Índice para a metade esquerda
+    j = 0  # Índice para a metade direita
+    
+    while i < len(metadeEsquerda) and j < len(metadeDireita):
+        if metadeEsquerda[i] <= metadeDireita[j]:
+            array.append(metadeEsquerda[i])
+            i += 1
+        else:
+            array.append(metadeDireita[j])
+            j += 1
+    
+    while i < len(metadeEsquerda):
+        array.append(metadeEsquerda[i])
+        i += 1
+    
+    while j < len(metadeDireita):
+        array.append(metadeDireita[j])
+        j += 1
+    
+    return array
+
+array = [64, 34, 25, 12, 22, 11, 90]
+sortedArray = mergeSort(array)
+
+print("Array ordenado:")
+print(*sortedArray)
