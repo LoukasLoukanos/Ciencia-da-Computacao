537dd66a85750b5ccf9e84a7aba41b0276add9fb
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-07-20 20:43:24 -0300

    Algoritmos 11

---
+++ Complementar - Estruturas de dados (Didático)/Algoritmos11/Alg11.java
@@ -0,0 +1,37 @@
+/* Algoritmo da Estrutura de dados linear estática (não dinâmica)
+   de Pesquisa/Busca buscaBinaria para realização de busca binária */
+
+public class Alg11 {
+
+    public static int buscaBinaria(int[] array, int valorProcurado) {
+        int inicio = 0;
+        int fim = array.length - 1;
+
+        while (inicio <= fim) {
+            int meio = inicio + (fim - inicio) / 2;
+
+            if (array[meio] == valorProcurado) {
+                return meio; // Retorna o índice do valor procurado
+            } else if (array[meio] < valorProcurado) {
+                inicio = meio + 1;
+            } else {
+                fim = meio - 1;
+            }
+        }
+
+        return -1; // Retorna -1 se o valor não foi encontrado
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        int valorProcurado = 11;
+
+        int indiceEncontrado = buscaBinaria(array, valorProcurado);
+
+        if (indiceEncontrado != -1) {
+            System.out.println("Valor " + valorProcurado + " encontrado no índice: " + indiceEncontrado);
+        } else {
+            System.out.println("Valor " + valorProcurado + " não encontrado no array.");
+        }
+    }
+}
---
+++ Complementar - Estruturas de dados (Didático)/Algoritmos11/Alg11.php
@@ -0,0 +1,35 @@
+<?php
+/* Algoritmo da Estrutura de dados linear estática (não dinâmica)
+   de Pesquisa/Busca buscaBinaria para realização de busca binária */
+
+function buscaBinaria($array, $valorProcurado) {
+    $inicio = 0;
+    $fim = count($array) - 1;
+
+    while ($inicio <= $fim) {
+        $meio = floor(($inicio + $fim) / 2);
+
+        if ($array[$meio] == $valorProcurado) {
+            return $meio; // Retorna o índice do valor procurado
+        } else if ($array[$meio] < $valorProcurado) {
+            $inicio = $meio + 1;
+        } else {
+            $fim = $meio - 1;
+        }
+    }
+
+    return -1; // Retorna -1 se o valor não foi encontrado
+}
+
+$array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+$valorProcurado = 11;
+
+$indiceEncontrado = buscaBinaria($array, $valorProcurado);
+
+if ($indiceEncontrado !== -1) {
+    echo "Valor $valorProcurado encontrado no índice: $indiceEncontrado";
+} else {
+    echo "Valor $valorProcurado não encontrado no array.";
+}
+
+?>
