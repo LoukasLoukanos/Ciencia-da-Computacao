204d0b7ee0b4e0dbe8e058c73d5af114c038891e
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2024-04-01 16:24:41 -0300

    Algoritmos 27

---
+++ Complementar - Estruturas de dados (Didático)/Algoritmos27/Alg27.php
@@ -0,0 +1,8 @@
+<?php
+/*
+♦ Algoritmo 27: Estrutura de Dados Não Linear Estática (não Dinâmica) de Grafo - Busca em Profundidade (Depth-First Search - DFS).
+*/
+
+
+
+?>
---
+++ Complementar - Estruturas de dados (Didático)/Algoritmos27/Alg27.py
@@ -0,0 +1,5 @@
+'''
+♦ Algoritmo 27: Estrutura de Dados Não Linear Estática (não Dinâmica) de Grafo - Busca em Profundidade (Depth-First Search - DFS).
+'''
+
+
