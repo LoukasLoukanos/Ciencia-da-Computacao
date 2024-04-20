a9f3d4194d2e2362d0db20404cf1d9cc5904d04a
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2024-04-20 12:05:27 -0300

    Algoritmos 02

--- Complementar - Estruturas de dados (Didático)/Algoritmos02/Alg02.rb
+++ Complementar - Estruturas de dados (Didático)/Algoritmos02/Alg02.rb
@@ -1,13 +1,5 @@
 =begin 
-♦ Estrutura de Dados Linear Estática de Fila Circular e Não-Circular (regra de acesso FIFO) de tipo de dados genérico: Algoritmo 02.
-→ Operações:
-    • isEmpty - verificar se a fila está vazia;
-    • isFull - verificar se a fila está cheia;
-    • enqueue - adicionar elementos;
-    • dequeue - remover o elemento da frente da fila;
-    • size - obter o tamanho atual da fila;
-    • front - acessar o elemento da frente da fila sem removê-lo;
-    • showQueue - mostar elementos da fila.
+♦ Algoritmo 02: Estrutura de Dados Linear Estática de Fila Circular e Não-Circular: Queue (Fila).
 
 obs: Em uma fila circular é necessário administrar os elementos na fila em relação à exclusão de espaços 
     livres do início, para obter, de forma circular, mais espaços no fim, para adição de novos elementos.
